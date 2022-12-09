84694fa960e6919f044cc4f6a6ab690a5e26ce88
José Romero <jose.romero@volvocars.com>
2022-12-09 11:24:44 +0100

    Added basic configuration and removed sensitive data

--- clients.py
+++ clients.py
@@ -3,8 +3,6 @@ from utils import sessions
 from telethon.sessions import StringSession
 from typing import List
 
-api_id = 1267461
-api_hash = '168acb04f099beb551aff4069d029320'
 
 def clients():
    result :List[CwClient] = [v for (k, v) in globals().items() if type(v) == CwClient]
